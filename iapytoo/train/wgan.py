ea5572d789bd143dcb274b5967341709c3cb0dbb
Lecomte Jean-François <jean-francois.lecomte@ifpen.fr>
2024-06-03 17:28:37 +0200

    first full version of wgan

--- iapytoo/train/wgan.py
+++ iapytoo/train/wgan.py
@@ -38,13 +38,15 @@ class WGAN(Training):
 
     # Fonction pour la pénalité de gradient
     def gradient_penalty(self, real_data, fake_data):
-        batch_size = real_data.size(0)
-        alpha = torch.rand(batch_size, 1).expand_as(real_data)
+        b_size = real_data.size(0)
+        alpha = torch.Tensor(b_size, 1, 1).uniform_(0, 1).to(real_data.device)
         interpolates = alpha * real_data + (1 - alpha) * fake_data
         interpolates = interpolates.requires_grad_(True)
 
         d_interpolates = self.discriminator(interpolates)
-        fake = torch.ones(d_interpolates.size(), requires_grad=False)
+        fake = torch.ones(
+            d_interpolates.size(), requires_grad=False, device=real_data.device
+        )
 
         gradients = grad(
             outputs=d_interpolates,
@@ -60,20 +62,20 @@ class WGAN(Training):
         return gradient_penalty
 
     @property
-    def discriminator(self):
+    def generator(self):
         return self._models[WGAN_FCT.GENERATOR]
 
     @property
-    def generator(self):
+    def discriminator(self):
         return self._models[WGAN_FCT.DISCRIMINATOR]
 
     @property
     def g_optimizer(self):
-        return self._optimizers[WGAN_FCT.GENERATOR]
+        return self._optimizers[WGAN_FCT.GENERATOR].torch_optimizer
 
     @property
     def d_optimizer(self):
-        return self._optimizers[WGAN_FCT.DISCRIMINATOR]
+        return self._optimizers[WGAN_FCT.DISCRIMINATOR].torch_optimizer
 
     def _create_criterion(self):
         return None
@@ -99,23 +101,23 @@ class WGAN(Training):
         return [g_optimizer, d_optimizer]
 
     def _update_d(self, real_data):
-        real_data = real_data.to(self.device)
         noise_dim = self.config["noise_dim"]
         batch_size = self.config["batch_size"]
         lambda_gp = self.config["lambda_gp"]
 
-        noise = torch.randn(batch_size, noise_dim)
-        noise = noise.to(self.device)
-        fake_data = self.generator(noise).detach()
+        self.discriminator.zero_grad()
 
-        self.d_optimizer.zero_grad()
+        noise = torch.randn(batch_size, noise_dim, 1, device=self.device)
+        fake_data = self.generator(noise)
+
+        real_data_reshaped = real_data.view(batch_size, 1, -1).to(self.device)
 
         # Pertes pour les vrais et faux
-        d_real = self.discriminator(real_data)
+        d_real = self.discriminator(real_data_reshaped)
         d_fake = self.discriminator(fake_data)
 
         # Pénalité de gradient
-        gp = self.gradient_penalty(real_data, fake_data)
+        gp = self.gradient_penalty(real_data_reshaped, fake_data)
 
         # Perte totale du discriminateur
         d_loss = d_fake.mean() - d_real.mean() + lambda_gp * gp
@@ -129,11 +131,10 @@ class WGAN(Training):
         batch_size = self.config["batch_size"]
 
         # Mise à jour du générateur
-        noise = torch.randn(batch_size, noise_dim)
-        noise = noise.to(self.device)
-        fake_data = self.generator(noise)
+        noise = torch.randn(batch_size, noise_dim, 1, device=self.device)
 
-        self.g_optimizer.zero_grad()
+        self.generator.zero_grad()
+        fake_data = self.generator(noise)
 
         g_loss = -self.discriminator(fake_data).mean()
         g_loss.backward()
@@ -161,17 +162,20 @@ class WGAN(Training):
             with tqdm(loader, unit="batch", file=sys.stdout) as tepoch:
                 d_mean = self.loss(WGAN_FCT.DISCRIMINATOR)
                 g_mean = self.loss(WGAN_FCT.GENERATOR)
-                for real_data, _ in tepoch:
-                    tepoch.set_description(f"{description} {epoch}")
+                tepoch.set_description(f"{description} {epoch}")
+                for step, (real_data, _) in enumerate(tepoch):
+                    # update discriminator
                     d_loss = update_d(real_data)
-                    timer.tick()
-
                     d_mean.update(d_loss)
 
-                g_loss = update_g(self)
-                g_mean.update(g_loss)
+                    # update generator not so often
+                    if step % 5:
+                        g_loss = update_g()
+                        g_mean.update(g_loss)
 
-                tepoch.set_postfix(d_loss=d_mean.value, g_loss=g_mean.value)
+                    timer.tick()
+
+                    tepoch.set_postfix(d_loss=d_mean.value, g_loss=g_mean.value)
 
             timer.log()
             timer.stop()
@@ -189,7 +193,6 @@ class WGAN(Training):
 
         self._models = self._create_models(train_loader)
         self._optimizers = self._create_optimizers()
-        print(self.generator)
 
         checkpoint = CheckPoint(run_id)
         checkpoint.init(self)
