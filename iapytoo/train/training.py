aa937243b0d430d08a6ce91edd80a2c3a86e484c
vitalbre <eduardo.vital-brasil-lorenzo-fernandez@ifpen.fr>
2025-08-08 10:47:51 +0200

    Corrected formatting

--- iapytoo/train/training.py
+++ iapytoo/train/training.py
@@ -317,8 +317,7 @@ class Training(Inference):
         train_time.stop()
 
     def fit(
-        self, train_loader: DataLoader, valid_loader: DataLoader,
-        run_id=None
+        self, train_loader: DataLoader, valid_loader: DataLoader, run_id=None
     ):
         num_epochs = self._config.training.epochs
 
